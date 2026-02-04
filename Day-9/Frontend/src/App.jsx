c08bf88f52c67175e0de7f283b1405c418c9a07a
Rahul <rahul251035@gmail.com>
2026-02-04 20:34:29 +0530

    api update

--- Day-9/Frontend/src/App.jsx
+++ Day-9/Frontend/src/App.jsx
@@ -10,7 +10,7 @@ function App() {
   const [notes, setNotes] = useState([])
 
   function fetchNotes() {
-    axios.get('http://localhost:3000/api/notes')
+    axios.get('https://note-app-3uqe.onrender.com/api/notes')
       .then((res) => {
         setNotes(res.data.notes)
       })
@@ -25,7 +25,7 @@ function App() {
 
     const { title, description } = e.target.elements
 
-    axios.post('http://localhost:3000/api/notes', {
+    axios.post('https://note-app-3uqe.onrender.com/api/notes', {
       title: title.value,
       description: description.value
     })
@@ -39,7 +39,7 @@ function App() {
   }
 
   function handleDelete(noteId) {
-    axios.delete('http://localhost:3000/api/notes/' + noteId)
+    axios.delete('https://note-app-3uqe.onrender.com/api/notes/' + noteId)
       .then(res => {
         fetchNotes()
       })
@@ -53,7 +53,7 @@ function App() {
     
   }
 function handleUpdateDone() {
-  axios.patch(`http://localhost:3000/api/notes/${updateId}`, {
+  axios.patch(`https://note-app-3uqe.onrender.com/api/notes/${updateId}`, {
     title: updateTitle,
     description: updateDescription
   })
