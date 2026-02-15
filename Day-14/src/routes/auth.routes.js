b5a200fe87539db921902296e1ee6e190f0ac578
Rahul <rahul251035@gmail.com>
2026-02-15 19:42:28 +0530

    Controller and auth insta-clone

--- Day-14/src/routes/auth.routes.js
+++ Day-14/src/routes/auth.routes.js
@@ -1,36 +1,9 @@
 const express = require('express')
-
+const authController = require('./../controllers/auth.controller')
 const authRouter = express.Router()
 
-authRouter.post('/register', async(req,res) =>{
-    const {email,username,password,bio,profileImage} = req.body
-    // const isUserExistsByEmail = await userModel.findOne({ email })
+authRouter.post('/register', authController.registerController) 
 
-    // if(isUserExistsByEmail) {
-    //     return res.status(409).json({
-    //         message: 'user already exists by username'
-    //     })
-    // }
+authRouter.post('/login', authController.loginController)
 
-    // const isUserExistsByUsername = await userModel.findOne({ username })
-
-    // if(isUserExistsByUsername) {
-    //     return res.status(409).json({
-    //         message: 'user already exist by usrname'
-    //     })
-    // }
-
-    const isUserAlreadyExists = await userModel.findOne({
-        $or: [
-            {username},
-            { email }
-        ]
-    })
-})
-
-if(isUserAlreadyExists) {
-    return res.status(409)
-    .json({
-        message: 'user already exists' + 
-    })
-}
+module.exports = authRouter
